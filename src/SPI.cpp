d2eaea5aed721d3d018c1121afa9cdabda11e7b7
Lou Amadio <lou@polyhobbyist.com>
2022-06-20 13:18:49 -0700

    ToF implementation

--- src/SPI.cpp
+++
@@ -1,4 +0,0 @@
-#include "Arduino.h"
-#include "SPI.h"
-
-SPIClass SPI;
--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,9 @@ SOFTWARE.
 #include <math.h>
 #include "rclcpp/rclcpp.hpp"
 #include <sensor_msgs/point_cloud2_iterator.hpp>
+#include <visualization_msgs/msg/marker.hpp>
+#include <geometry_msgs/msg/point.hpp>
+
 
 #include "Arduino.h"
 #include "Wire.h"
@@ -36,6 +39,13 @@ SOFTWARE.
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
+
+constexpr uint8_t kResolution = 8;
+
+constexpr inline float DegToRad(float deg)
+{
+  return (deg * M_PI) / 180.0f;
+}
 
 class I2CPublisher : public rclcpp::Node
 {
@@ -49,20 +59,31 @@ class I2CPublisher : public rclcpp::Node
 
     void initialize()
     {
-      get_parameter_or<uint8_t>("id", _id, 0x52); 
+      get_parameter_or<uint8_t>("id", _id, 0x29); 
+      get_parameter_or<std::string>("frame_id", _frameId, "depth"); 
+      get_parameter_or<std::string>("topic", _topic, "points2"); 
+      get_parameter_or<double>("poll", _poll, 500.0);
+      get_parameter_or<bool>("debug", _debug, false);
+
+      
 
       Wire.begin();
+      Wire.setAddressSize(2);
       myImager.begin();
       
-      myImager.setResolution(8*8); //Enable all 64 pads
+      myImager.setResolution(kResolution * kResolution); //Enable all 64 pads
       
-      _width = sqrt(myImager.getResolution()); //Calculate printing width
       myImager.startRanging();
 
-      _pointcloud = this->create_publisher<sensor_msgs::msg::PointCloud2>("points2", 1);    // todo configurable
+      _pointcloud = this->create_publisher<sensor_msgs::msg::PointCloud2>(_topic, 1);
+      if (_debug)
+      {
+        _marker = this->create_publisher<visualization_msgs::msg::Marker>("tof_debug", 1);
+      }
   
-      // TODO: make this rate configurable
-      _timer = this->create_wall_timer(500ms, std::bind(&I2CPublisher::timer_callback, this));
+      _timer = this->create_wall_timer(
+        std::chrono::duration<double, std::milli>(_poll), 
+        std::bind(&I2CPublisher::timer_callback, this));
     }
 
   private:
@@ -77,10 +98,10 @@ class I2CPublisher : public rclcpp::Node
         {
           auto point_cloud = sensor_msgs::msg::PointCloud2();
 
-          point_cloud.header.frame_id = "depth";   // todo frame id 
+          point_cloud.header.frame_id = _frameId;
           point_cloud.header.stamp = rclcpp::Clock().now();
-          point_cloud.height = _width;
-          point_cloud.width = _width;
+          point_cloud.height = kResolution;
+          point_cloud.width = kResolution;
           point_cloud.is_dense = false;
           point_cloud.is_bigendian = false;
 
@@ -93,28 +114,26 @@ class I2CPublisher : public rclcpp::Node
 
           pcd_modifier.resize(point_cloud.height * point_cloud.width);
 
-          for (size_t w = 0; w < _width; w++)
+          for (size_t w = 0; w < kResolution; w++)
           {
-            for (size_t h = 0; h < _width; h++)
+            for (size_t h = 0; h < kResolution; h++)
             {
-              float z = static_cast<float>(measurementData.distance_mm[w + (_width * h)]);
+              float depth = static_cast<float>(measurementData.distance_mm[w + (kResolution * h)]);
 
-              if (z <= 0.0f)
+              if (depth <= 0.0f)
               {
                 *iter_x = *iter_y = *iter_z = std::numeric_limits<float>::quiet_NaN();
               }
               else
               {
-                constexpr float kMillimeterToMeter = 1.0 / 1000.0f;
-                constexpr float kFovDeg = 63.0f;
-                constexpr float kFov = (kFovDeg * 180.0f) / M_PI;
-                float fovPerPixel = kFov / static_cast<float>(_width);
+                constexpr float kMillimeterToMeter = 1.0f / 1000.0f;
+                constexpr float kFov = DegToRad(45.0f);
+                float fovPerPixel = kFov / static_cast<float>(kResolution);
 
-
-                *iter_x = kMillimeterToMeter * static_cast<float>(sin(w * fovPerPixel - kFov / 2.0f)) * z;
-                *iter_y = kMillimeterToMeter * static_cast<float>(sin(h * fovPerPixel - kFov / 2.0f)) * z;
-                *iter_z = kMillimeterToMeter * z;
-              }
+                *iter_x = static_cast<float>(kMillimeterToMeter * cos(w * fovPerPixel - kFov / 2.0f - DegToRad(90.0f)) * depth);
+                *iter_y = static_cast<float>(kMillimeterToMeter * sin(h * fovPerPixel - kFov / 2.0f) * depth);
+                *iter_z = static_cast<float>(kMillimeterToMeter * depth);
+              } 
 
               ++iter_x; ++iter_y; ++iter_z;
             }
@@ -122,17 +141,49 @@ class I2CPublisher : public rclcpp::Node
 
           _pointcloud->publish(point_cloud);
 
+          if (_debug)
+          {
+            visualization_msgs::msg::Marker marker;
+            marker.header.frame_id = _frameId;
+            marker.header.stamp = rclcpp::Time();
+            marker.id = 1;
+            marker.type = visualization_msgs::msg::Marker::ARROW;
+            marker.action = visualization_msgs::msg::Marker::ADD;
+            marker.mesh_use_embedded_materials = true;
+            marker.pose.position.x = 0;
+            marker.pose.position.y = 0;
+            marker.pose.position.z = 0;
+            marker.pose.orientation.x = 0.0;
+            marker.pose.orientation.y = 0.0;
+            marker.pose.orientation.z = 0.0;
+            marker.pose.orientation.w = 1.0;          
+            marker.scale.x = 0.01;
+            marker.scale.y = 0.012;
+            marker.scale.z = 0.0;
+            marker.color.a = 1.0;
+            marker.color.r = 0.0;
+            marker.color.g = 0.0;
+            marker.color.b = 1.0;
+
+            geometry_msgs::msg::Point pt;
+            marker.points.push_back(pt);
+            pt.z = .1;
+            marker.points.push_back(pt);
+
+            _marker->publish(marker);
+          }
         }
       }
     }
     rclcpp::TimerBase::SharedPtr _timer;
 
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr _pointcloud;
-
-    uint8_t _width;
-  
+    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr _marker;
     uint8_t _id;
-
+    double _poll;
+    std::string _topic;
+    std::string _frameId;
+    bool _debug;
 };
 
 int main(int argc, char * argv[])
@@ -141,6 +192,10 @@ int main(int argc, char * argv[])
 
     auto node = std::make_shared<I2CPublisher>();
     node->declare_parameter("i2c_address");
+    node->declare_parameter("frame_id");
+    node->declare_parameter("poll");
+    node->declare_parameter("topic");
+    node->declare_parameter("debug");
 
     node->initialize();
 
